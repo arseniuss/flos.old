3d027531675375bf69bb300db1565a3deae16998
Armands Skolmeisters (arseniuss) <arseniuss@arseniuss.id.lv>
2015-01-29 02:26:47 +0200

    Pre-commit hook

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -10,11 +10,13 @@
 #* @copyright see file LICENSE
 #*
 
+echo "Pre-commit hook: formating source files"
+
 version=`gnuindent --version 2>/dev/null`
 if test "x$version" = "x"; then
 	version=`indent --version 2>/dev/null`
 	if test "x$version" = "x"; then
-		echo "GStreamer git pre-commit hook:"
+		echo "git pre-commit hook:"
 		echo "Did not find GNU indent, please install it before continuing."
 		exit 1
 	fi
@@ -34,19 +36,54 @@ case `$INDENT --version` in
 	;;
 esac
 
-INDENT_PARAMETERS="--braces-on-if-line \
-	--case-brace-indentation0 \
-	--case-indentation2 \
-	--braces-after-struct-decl-line \
-	--line-length80 \
-	--no-tabs \
-	--cuddle-else \
-	--dont-line-up-parentheses \
-	--continuation-indentation4 \
-	--honour-newlines \
-	--tab-size8 \
-	--indent-level2 \
-	--leave-preprocessor-space"
+INDENT_PARAMETERS="\
+        --braces-after-struct-decl-line \
+        --braces-on-func-def-line \
+        --braces-on-if-line \
+        --break-after-boolean-operator \
+        --case-brace-indentation0 \
+        --case-indentation4 \
+        --comment-delimiters-on-blank-lines \
+        --comment-indentation40 \
+        --continuation-indentation4 \
+        --continue-at-parentheses \
+        --cuddle-else \
+        --dont-break-procedure-type \
+        --dont-format-comments \
+        --honour-newlines \
+        --indent-level4 \
+        --leave-optional-blank-lines \
+        --leave-preprocessor-space \
+        --line-length80 \
+        --no-parameter-indentation \
+        --no-space-after-casts \
+        --no-space-after-for \
+        --no-space-after-function-call-names \
+        --no-space-after-if \
+        --no-space-after-parentheses \
+        --no-space-after-while \
+        --no-tabs \
+        --preprocessor-indentation4 \
+        --start-left-side-of-comments \
+        --tab-size4 \
+        "
 
+__indent_C() {
+        if test ! -f $1
+        then
+            return;
+        fi
+ 
+        "$INDENT" $INDENT_PARAMETERS "$1"
+        git add "$1"
+}
 
-
+find . -not -path './gcc-cc/*' -not -path './.*' | while read file;
+do 
+    ext=$(expr "$file" : ".*\(\..*\)")
+    case $ext in
+    .c|.h)
+        __indent_C $file
+    ;;
+    esac
+done
